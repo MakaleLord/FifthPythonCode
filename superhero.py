4b82eb535c624e44269e2f73363bb0102523c3ae
Makale Lord <110202582+MakaleLord@users.noreply.github.com>
2022-07-28 18:39:12 -0500

    Add files via upload

---
+++ superhero.py
@@ -0,0 +1,74 @@
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+html{
+   display:flex;
+   justify-content:center;
+   align-items: center;
+   width: 100%;
+   height: 100%;
+   background-image: url('https://source.unsplash.com/collection/327760/1600x900');
+   background-size:cover;
+}
+
+body{
+   width:55%;
+   height:60vh;
+   border:1px solid lightgray;
+   background-color:white;
+   border-radius:8px;
+   font-family: 'Roboto Mono', monospace;
+   box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+   border-top:32px solid #eee;
+   overflow-y:scroll;
+}
+
+body > div{
+  height: calc(100% - 40px);
+  overflow-y:auto;
+  padding: 20px;
+  font-size: 24px;
+  white-space: pre;
+}
+
+pre{
+  margin:inherit;
+  font-family: inherit;
+}
+</style>
+
+<div>
+Avengers
+_ Ant-Man
+_ Black Panther
+_ Captain America
+_ Doctor Strange
+_ Hulk
+_ Iron Man
+_ Spider-Man
+
+You are  Doctor Strange
+
+------
+Justice League
+_ Aquaman
+_ Bat-man
+_ Cyborg
+_ Flash
+_ Superman
+_ Wonder-woman
+
+You are  Superman
+
+------
+Teen Titans
+_ Beast Boy
+_ Raven
+_ Robin
+_ Starfire
+_ Terra
+
+You are  Terra
+
+------
+You are  Black Panther
